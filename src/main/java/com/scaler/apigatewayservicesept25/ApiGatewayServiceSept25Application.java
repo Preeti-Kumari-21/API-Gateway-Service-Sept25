7d0f79be7c3dc4b1ad8bcb578c9a1f7796371e26
Preeti <preeti.kumari241116@gmail.com>
2025-09-17 20:10:37 +0530

    first commit

---
+++ src/main/java/com/scaler/apigatewayservicesept25/ApiGatewayServiceSept25Application.java
@@ -0,0 +1,13 @@
+package com.scaler.apigatewayservicesept25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServiceSept25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceSept25Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/apigatewayservicesept25/ApiGatewayServiceSept25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.apigatewayservicesept25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceSept25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
